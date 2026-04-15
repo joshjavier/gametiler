981fe7a799f330c2e48c6b20ff7cf5648edf5d0d
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2026-04-15 11:09:22 +0800

    feat: add styles to CasinoLabelPicker component

--- components/CasinoLabelPicker/index.tsx
+++ components/CasinoLabelPicker/index.tsx
@@ -22,23 +22,27 @@ export function CasinoLabelPicker() {
 
   return (
     <Select.Root items={casinoLabels} value={selectedLabel} onValueChange={setSelectedLabel}>
-      <Select.Trigger className="flex min-w-30 cursor-pointer items-center justify-between gap-1.25 rounded-(--border-radius-md) border-[0.5px] border-(--color-border-secondary) bg-(--color-background-secondary) px-2.25 py-1 text-[12px] font-medium text-(--color-text-primary)">
-        <Select.Value placeholder="Select a brand" />
-        <Select.Icon>
+      <Select.Trigger className="flex min-w-28 cursor-pointer items-center justify-between gap-1.25 rounded-(--border-radius-md) border-[0.5px] border-(--color-border-secondary) bg-(--color-background-secondary) px-2.25 py-1 text-[12px] font-medium text-(--color-text-primary)">
+        <Select.Value placeholder="Select a label" className="data-placeholder:opacity-60" />
+        <Select.Icon className="data-popup-open:rotate-180">
           <CaretDownIcon size={10} weight="bold" />
         </Select.Icon>
       </Select.Trigger>
 
       <Select.Portal>
-        <Select.Positioner alignItemWithTrigger={false}>
-          <Select.Popup>
+        <Select.Positioner alignItemWithTrigger={false} sideOffset={4}>
+          <Select.Popup className="min-w-(--anchor-width) origin-(--transform-origin) rounded-(--border-radius-md) border-[0.5px] border-(--color-border-secondary) bg-(--color-background-secondary) py-1 shadow-md transition outline-none data-ending-style:scale-90 data-ending-style:opacity-0 data-starting-style:scale-90 data-starting-style:opacity-0">
             <Select.List>
               {casinoLabels.map(({ label, value }) => (
-                <Select.Item key={label} value={value}>
-                  <Select.ItemIndicator>
+                <Select.Item
+                  key={label}
+                  value={value}
+                  className="grid cursor-pointer grid-cols-[12px_1fr] items-center gap-1.25 px-2.25 py-1 text-[12px] font-medium text-(--color-text-primary) outline-none hover:bg-(--color-border-secondary) data-highlighted:bg-(--color-border-secondary)"
+                >
+                  <Select.ItemIndicator className="flex w-3 items-center">
                     <CheckIcon size={12} weight="bold" />
                   </Select.ItemIndicator>
-                  <Select.ItemText>{label}</Select.ItemText>
+                  <Select.ItemText className="col-start-2">{label}</Select.ItemText>
                 </Select.Item>
               ))}
             </Select.List>
