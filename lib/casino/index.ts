48f6ba94d8b0def0ae714f9a07864416306439b4
Josh Javier <46987872+joshjavier@users.noreply.github.com>
2026-04-16 10:23:21 +0800

    fix: remove caching of API responses

--- lib/casino/index.ts
+++ lib/casino/index.ts
@@ -94,11 +94,7 @@ export function fetchGames(
     request.headers.set('user-agent', requireEnv('MOBILE_UA'));
   }
 
-  return fetch(request, {
-    next: {
-      revalidate: 300, // cache response for 5 minutes
-    },
-  });
+  return fetch(request);
 }
 
 export function metadataToGame({ sid, game, name, provider }: GameMetaData): Game {
